75f0b9867498ff319885ea456b61a37a52fdd3ee
enjelitabrlingga <enjelitabrlingga@gmail.com>
2026-05-14 20:48:03 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+	return "Ini adalah halaman About";
+});
+
+
+Route::get('/user/{name}', function ($name)
+ { return "Halo, $name!";
+});
+	
