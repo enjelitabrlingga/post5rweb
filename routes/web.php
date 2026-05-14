d4b204a07098d61f3ca994bcf542db4632ae2895
enjelitabrlingga <enjelitabrlingga@gmail.com>
2026-05-15 01:18:21 +0700

    Update routing 5 route

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function (){
-    return "Ini adalah halaman About";
+Route::get('/about', function () {
+    return "Ini adalah halaman about";
 });
 
-Route::get('/user/{name}', function ($name)
-{ return "Halo, $name";
+Route::get('/user/{nama}', function ($nama) {
+    return "Halo, $nama";
+});
 
+
+//route tanpa parameter
+
+Route::get('/about', function () {
+    return 'About - Belajar Routing Laravel';
+});
+
+Route::get('/', function () {
+    return '🏠 Home - Praktikum Laravel 11';
+});
+
+//route dengan parameter
+
+Route::get('/user/{name}', function ($name) {
+    return "Selamat datang, $name 👋";
+});
+
+Route::get('/angkatan/{tahun}', function ($tahun) {
+    return "📅 Angkatan: $tahun - Fakultas Teknologi Informasi";
+});
+
+Route::get('/kategori/{jenis}', function ($jenis) {
+    return "📦 Kategori Produk: $jenis";
 });
